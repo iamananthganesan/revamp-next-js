d03880e24039d77c1b0592d4ec346cba285e6442
iamananthganesan <mail2ananthece@gmail.com>
2025-08-10 17:43:27 +0530

    next auth implementation

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // async redirects(){
-    //     return [
-    //         {
-    //             source: '/blogs',
-    //             destination: "/about",
-    //             permanent: true
-    //         }
-    //     ]
-    // }
+
 };
 
 export default nextConfig;
